d32e6bb9ba8f096f2404fa7a08b59b97b6f541c2
Sourav0010 <mohantysourav656@gmail.com>
2025-11-02 22:00:36 +0530

    Feat: Connected one backend to two applications

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useQuery } from 'convex/react';
-import { api } from '@workspace/backend/convex/_generated/api';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
    const users = useQuery(api.users.getMany);
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from '@workspace/ui/components/button';
+'use client';
+import { useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
+   const users = useQuery(api.users.getMany);
    return (
       <div className='flex items-center justify-center min-h-svh'>
          <div className='flex flex-col items-center justify-center gap-4'>
-            <h1 className='text-2xl font-bold'>Hello Dev</h1>
-            <Button size='sm'>Button</Button>
+            <h1 className='text-2xl font-bold'>app/widget</h1>
+            <p>{JSON.stringify(users)}</p>
          </div>
       </div>
    );
