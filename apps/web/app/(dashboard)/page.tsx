3e1fc5cb2e1c84fbfd7c7ec79e29bf0d88e658fa
Sourav0010 <mohantysourav656@gmail.com>
2025-11-06 16:13:21 +0530

    Organization

---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+import {
+   useMutation,
+   useQuery,
+   Authenticated,
+   Unauthenticated,
+} from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
+import { Button } from '@workspace/ui/components/button';
+import { OrganizationSwitcher, SignInButton, UserButton } from '@clerk/nextjs';
+
+export default function Page() {
+   const users = useQuery(api.users.getMany);
+   const addUser = useMutation(api.users.add);
+   return (
+      <>
+         <div className='flex items-center justify-center min-h-svh'>
+            <div className='flex flex-col items-center justify-center gap-4'>
+               <UserButton />
+               <OrganizationSwitcher hidePersonal={true} />
+               <Button onClick={() => addUser()}>Add</Button>
+               <h1 className='text-2xl  font-bold'>app/web</h1>
+               <p className='text-center'>{JSON.stringify(users)}</p>
+            </div>
+         </div>
+      </>
+   );
+}
